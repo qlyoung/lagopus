25f5750f09fb8050cae62c1f3e6b8a846cd73357
Quentin Young <qlyoung@qlyoung.net>
2020-01-26 01:48:03 +0000

    fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+WD=$(pwd)
+
+cd docker-images/lagopus-fuzzer
+SHA=$(docker build -q . | cut -d':' -f2)
+echo "Tagging fuzzer ($(pwd))"
+docker tag $SHA qlyoung/lagopus-fuzzer:latest
+docker push qlyoung/lagopus-fuzzer:latest
+cd $WD
+
+cd docker-images/lagopus-jobserver
+docker build .
+SHA=$(docker build -q . | cut -d':' -f2)
+echo "Tagging jobserver ($(pwd))"
+docker tag $SHA qlyoung/lagopus-jobserver:latest
+docker push qlyoung/lagopus-jobserver:latest
+cd $WD
+
+rm lagopus.yaml
 {
 for file in ./jobserver/*.yaml; do
 	cat $file
