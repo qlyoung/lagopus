e5ec3fbecc040ff8e832bc4991f263e99c024222
Quentin Young <qlyoung@qlyoung.net>
2020-01-27 05:58:55 +0000

    docker-images: remove unused lagopus-jobserver image

--- build.sh
+++ build.sh
@@ -8,14 +8,6 @@ docker tag $SHA qlyoung/lagopus-fuzzer:latest
 docker push qlyoung/lagopus-fuzzer:latest
 cd $WD
 
-cd docker-images/lagopus-jobserver
-docker build .
-SHA=$(docker build -q . | cut -d':' -f2)
-echo "Tagging jobserver ($(pwd))"
-docker tag $SHA qlyoung/lagopus-jobserver:latest
-docker push qlyoung/lagopus-jobserver:latest
-cd $WD
-
 rm lagopus.yaml
 {
 for file in ./jobserver/*.yaml; do
