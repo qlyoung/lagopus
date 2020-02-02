df1f611cdadd2701eac3bf69b99af8e29b0ab6cd
Quentin Young <qlyoung@qlyoung.net>
2020-02-02 23:22:19 +0000

    fuzzer, scanner: collect crash output
    lagopus-fuzzer:
    - run each crashing input to collect crash output
    - add a new table to crashes.db containing the output for each sample
    - don't remove supposedly non-crashing inputs; this seems buggy
    - add sqlite3 to deps
    
    lagopus-scanner:
    - export crash output from crashes.db
    - fix some inaccurate comments
    - split out some functionality into smaller functions
    - add a few arguments to set job directory, skip database export, and do
      oneshot scans
    
    lagopus-db:
    - change `backtrace` column to type longtext, to support storing program
      output
    - rename `type` column to `description`, because it sometimes is that,
      and make it a tinytext to support this
    - increase `exploitability` field to size 32, because at least one class
      name is over 20 chars
    
    lagopus-server:
    - update DataTables query for changed fields
    
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- docker-images/lagopus-db/schema.sql
+++ docker-images/lagopus-db/schema.sql
@@ -11,10 +11,10 @@ CREATE TABLE `jobs` (
   ) ENGINE=InnoDB;
 CREATE TABLE `crashes` (
     `job_id` varchar(128) NOT NULL,
-    `type` varchar(64),
-    `exploitability` varchar(20),
+    `description` tinytext,
+    `exploitability` varchar(32),
     `sample_path` varchar(4096) NOT NULL,
-    `backtrace` varchar(4096) NOT NULL,
+    `backtrace` longtext NOT NULL,
     `backtrace_hash` char(65),  # two md5s plus a period
     PRIMARY KEY (`job_id`, `backtrace_hash`)
   ) ENGINE=InnoDB;
