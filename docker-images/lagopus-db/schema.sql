1346ba1f733dd7f9f56e9293e232e419ee32910c
Quentin Young <qlyoung@qlyoung.net>
2020-02-02 03:29:24 +0000

    *: display crashes in web app
    Display crashes in the crashes page.
    
    lagopus-db:
    - Change db schema to use consistent name for job id field (job_id)
    
    lagopus-scanner:
    - Update code to match job_id field change
    
    lagopus-server:
    - Update code to match job_id field change
    - Add API endpoint to get crashes
    - Remove placeholder data from crashes.html, fetch crashes from API
    
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- docker-images/lagopus-db/schema.sql
+++ docker-images/lagopus-db/schema.sql
@@ -1,21 +1,21 @@
 USE lagopus;
 CREATE TABLE `jobs` (
-    `name` varchar(128) NOT NULL,
+    `job_id` varchar(128) NOT NULL,
     `driver` varchar(20) NOT NULL,
     `target` varchar(4096) NOT NULL,
     `cores` int(11) NOT NULL,
     `memory` int(11) NOT NULL,
     `deadline` int(11) NOT NULL,
     `start_time` timestamp,
-    PRIMARY KEY (`name`)
+    PRIMARY KEY (`job_id`)
   ) ENGINE=InnoDB;
 CREATE TABLE `crashes` (
-    `job_name` varchar(128) NOT NULL,
+    `job_id` varchar(128) NOT NULL,
     `type` varchar(64),
     `exploitability` varchar(20),
     `sample_path` varchar(4096) NOT NULL,
     `backtrace` varchar(4096) NOT NULL,
     `backtrace_hash` char(65),  # two md5s plus a period
-    PRIMARY KEY (`job_name`, `backtrace_hash`)
+    PRIMARY KEY (`job_id`, `backtrace_hash`)
   ) ENGINE=InnoDB;
 
