36e4b46b72541eb815abe976e42c0bd942dd80f4
Quentin Young <qlyoung@qlyoung.net>
2020-04-26 19:22:55 -0400

    docs: add qt logo
    Signed-off-by: Quentin Young <qlyoung@qlyoung.net>

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Logo
+html_logo = '../lagopus.svg'
+
+# Favicon
+html_favicon = 'favicon-32x32.png'
